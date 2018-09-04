7b096130e9105f1255d9d82397f3e24a9ad30429
Andrew <>
2018-09-04 10:45:59 +0100

    Quickly making a duck game on coffee break

---
+++ src/views/Game.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/384ba7ef0c7f5c8b83121cecd8f26809.jpg">
+    <h1>Be nice to the duck</h1>
+    ><input v-model="message.text" @keyup.enter="submit">
+    <p>{{duck_reply}}</p>
+  </div>
+</template>
+
+<script>
+const axios = require('axios')
+const qs = require('qs')
+export default {
+  name: 'Game',
+  data () {
+    return {
+      message: {
+        text: this.message,
+        language: 'english'
+      },
+      response: null,
+      duck_reply: ''
+    }
+  },
+  methods: {
+    submit () {
+      const options = {
+        headers: {
+          'X-Mashape-Key': 'GtzH0B8PzkmshTizmQTqGadyOUHHp1euquSjsn7gAZY47UCfLi',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Accept': 'application/json'
+        }
+      }
+      axios.post('https://japerk-text-processing.p.mashape.com/sentiment/', qs.stringify(this.message), options).then(r => {
+        this.response = r.data.label
+      })
+    }
+  },
+  watch: {
+    'response': function (newVal) {
+      if (newVal === 'pos') {
+        this.duck_reply = 'You are being nice to the duck and he is happy'
+      }
+      if (newVal === 'neg') {
+        this.duck_reply = 'That wasn\'t very nice'
+      }
+      if (newVal === 'neutral') {
+        this.duck_reply = 'You said something to the duck. He\'s unsure how to take it'
+      }
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" @click="start">
+    <img alt="Vue logo" src="../assets/duck.0.png">
+    <p>Tap to begin</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  methods: {
+    start () {
+      this.$router.push({ path: '/game' })
+    }
   }
 }
 </script>
